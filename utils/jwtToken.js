26817de3104e40e6d216f3997a259d331c6e3a23
Shivam Saurabh <95334223+Shivam88881@users.noreply.github.com>
2023-12-23 12:16:21 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -10,6 +10,7 @@ const sendToken = (user,statusCode,res)=>{
           ),
           httpOnly:true,
           domain: '.netlify.app',
+          sameSite: 'None',
           
      };
      res.status(statusCode).cookie('token',token,options).json({
