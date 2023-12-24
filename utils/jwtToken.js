c91573ec9154599909d797fe573390e54f5bab42
Shivam Saurabh <95334223+Shivam88881@users.noreply.github.com>
2023-12-24 10:20:35 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -10,6 +10,7 @@ const sendToken = (user,statusCode,res)=>{
           ),
           httpOnly:true,
           sameSite: 'None',
+          domain:'netlify',
           secure: process.env.NODE_ENV === 'production',
           
      };
