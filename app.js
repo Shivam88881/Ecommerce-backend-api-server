8492a6edd226c2e8dce7a282096d9db34c569d56
Shivam Saurabh <95334223+Shivam88881@users.noreply.github.com>
2023-12-24 09:33:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 
 
 const corsOptions = {
-    origin: '*',
+    origin: 'http://localhost:3000',
     optionsSuccessStatus: 200,
     credentials: true  // Corrected property name
 };
