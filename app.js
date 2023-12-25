59bfb5d6b4c34bdd5df3213dd2b23326d668cb21
Shivam Saurabh <95334223+Shivam88881@users.noreply.github.com>
2023-12-25 13:33:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 
 
 const corsOptions = {
-    origin: 'http:localhost:3000',
+    origin: 'http://localhost:3000',
     optionsSuccessStatus: 200,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
