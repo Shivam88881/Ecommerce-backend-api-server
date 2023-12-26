82440eec86f4fe424a8ff4b05ba76dc0a21dd9c6
Shivam Saurabh <95334223+Shivam88881@users.noreply.github.com>
2023-12-26 22:39:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://regal-piroshki-c908ee.netlify.app',
     optionsSuccessStatus: 200,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
